53c7d5697d207ab580a9da058963c58058b79e0d
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-07 23:47:13 -0600

    Pruebas en App

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,20 @@
+import { ServerApp } from '../src/presentation/server-app';
 
 describe('Test in the app', () => { 
-    test('should first', () => { 
+    test('should call Server.run with values', async() => { 
+        const serverRunMock = jest.fn();
 
+        ServerApp.run = serverRunMock;
+        process.argv = ['node', 'app.ts', '-b', '10']
+
+        await import('../src/app');
+
+        expect( serverRunMock ).toHaveBeenCalledWith({
+            base: 10, 
+            destination: "outputs", 
+            limit: 10, 
+            name: "multiplication-table", 
+            showTable: false
+        });
     });
 })
